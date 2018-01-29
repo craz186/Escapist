2422623153f398ff708a89a108e54b5b7234d801
Sean Gibbons <sean.gibbons4@mail.dcu.ie>
2018-01-29 15:37:04 +0000

    ESCAPIST-1 Initial board and tile classes

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	// 2d array declaration in C#
+	Tile[,] tiles;
+	public Board(int length, int height) {
+		tiles = new Tile[length, height];
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,10 @@
+﻿public class Tile {
+    
+    private int x;
+    private int y;
+        
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
